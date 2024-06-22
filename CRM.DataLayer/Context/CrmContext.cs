c2a79ffce62737e8fa3e64ea5c59a6a0f8c6881a
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-06-22 17:35:35 +0330

    Add project files.

---
+++ CRM.DataLayer/Context/CrmContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.DataLayer.Context
+{
+    public class CrmContext : DbContext
+    {
+        public CrmContext(DbContextOptions<CrmContext> options) : base(options)
+        {
+                
+        }
+
+
+    }
+}
