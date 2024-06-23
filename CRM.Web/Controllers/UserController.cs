509b56aad8f680a5f37ce08e8cc70d0264fccb8a
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-06-23 22:38:45 +0330

    Added Create Customer And Create Marketer Html

--- CRM.Web/Controllers/UserController.cs
+++ CRM.Web/Controllers/UserController.cs
@@ -4,9 +4,42 @@ namespace CRM.Web.Controllers
 {
 	public class UserController : Controller
 	{
-		public IActionResult Index()
-		{
-			return View();
-		}
-	}
+        #region Actions
+
+        #region User List
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region Add User
+
+        #region Create Custome
+
+        [HttpGet]
+        public async Task<IActionResult> CreateCustomer()
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region Create Marketer
+
+        [HttpGet]
+        public async Task<IActionResult> CreateMarketer()
+        {
+            return View();
+        }
+
+        #endregion
+
+
+        #endregion
+
+        #endregion
+    }
 }
