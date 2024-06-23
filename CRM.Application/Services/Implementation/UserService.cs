dc2449ed5c08be66bff41ca546040ddbbe3ad074
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-06-23 22:51:24 +0330

    Added User Service And User Repository

---
+++ CRM.Application/Services/Implementation/UserService.cs
@@ -0,0 +1,13 @@
+﻿using CRM.Application.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.Application.Services.Implementation
+{
+    public class UserService : IUserService
+    {
+    }
+}
