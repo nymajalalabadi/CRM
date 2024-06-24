603a423003b5bafb55030c43b7171461dcaa53ae
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-06-24 10:56:14 +0330

    Added Paging

---
+++ CRM.Domain/ViewModels/User/FilterUserViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CRM.Domain.Entities.Account;
+using CRM.Domain.ViewModels.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.Domain.ViewModels.User
+{
+    public class FilterUserViewModel : Paging<Entities.Account.User>
+    {
+        public string? FilterMobile { get; set; }
+
+        public string? FilterFirstName { get; set; }
+
+        public string? FilterLastName { get; set; }
+    }
+
+}
