d6aac5d95f91bdf6c5b2c5202bd72778e4e46645
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-07-01 01:12:19 +0330

    Added FilterOrders Html

--- CRM.Web/Controllers/OrderController.cs
+++ CRM.Web/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
 ﻿using CRM.Application.Services.Interface;
+using CRM.Domain.ViewModels.Orders;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CRM.Web.Controllers
@@ -8,6 +9,7 @@ namespace CRM.Web.Controllers
         #region Constructor
 
         private readonly IOrderService _orderService;
+
         private readonly IUserService _userService;
 
         public OrderController(IOrderService orderService, IUserService userService)
@@ -18,8 +20,21 @@ namespace CRM.Web.Controllers
 
         #endregion
 
-        #region Actions
+        #region Filter Orders
 
+        public async Task<IActionResult> FilterOrders(FilterOrderViewModel filter)
+        {
+            var result = await _orderService.FilterOrder(filter);
+
+            return View(result);
+        }
+
+        #endregion
+
+
+        #region Create Order
+
+        [HttpGet]
         public async Task<IActionResult> CreateOrder(long id)
         {
             ViewBag.customer = await _userService.GetCustomerById(id);
@@ -32,6 +47,33 @@ namespace CRM.Web.Controllers
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder(CreateOrderViewModel orderViewModel)
+        {
+            ViewBag.customer = await _userService.GetCustomerById(orderViewModel.CustomerId);
+
+            if (!ModelState.IsValid)
+            {
+                ViewBag.customer = await _userService.GetCustomerById(orderViewModel.CustomerId);
+
+                TempData[WarningMessage] = "اطلاعات وارد شده صحیح نمی باشد";
+                return View(orderViewModel);
+            }
+
+            var result = await _orderService.CreateOrder(orderViewModel);
+
+            switch (result)
+            {
+                case CreateOrderResult.Success:
+                    TempData[SuccessMessage] = "عملیات با موفقیت انجام شد";
+                    return RedirectToAction("FilterOrders");
+                case CreateOrderResult.Fail:
+                    TempData[ErrorMessage] = "عملیات با شکست مواجه شد";
+                    break;
+            }
+
+            return View(orderViewModel);
+        }
 
         #endregion
     }
